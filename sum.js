8292df97fc87689347e228ea2346286785132a1a
Dominic Tremblay <dominictremblay24@gmail.com>
2023-11-14 14:59:04 -0500

    solving the full sum problem

--- sum.js
+++ sum.js
@@ -8,11 +8,11 @@
 // What's the type of our command line args?
 const args = process.argv.slice(2);
 console.log('args:', args);
-
 // Edge cases: We need at least 2 arguments. => output error message
-
-// Edge cases: We need to check if they are whole numbers
-// Edge cases: If any argument is not a number, output an error message.
+if (args.length < 2) {
+  console.log('Error: please, enter at least 2 numbers');
+  process.exit();
+}
 
 // iterate through the args
 // c-style loop
@@ -28,9 +28,22 @@ let total = 0;
 
 for (let arg of args) {
   // Add each argument
-  total += Number(arg); // typecasting
-  console.log('arg:', Number(arg), 'Total:', total);
+  // Edge cases: We need to check if they are whole numbers
+  // Number.isInteger()
+  // Number(arg) % 1 === 0
+  // Edge cases: If any argument is not a number, output an error message.
+
+  if (isNaN(Number(arg))) {
+    console.log('Error: please, input only numbers');
+    // stop the execution
+    process.exit();
+  }
+
+  if (Number.isInteger(Number(arg))) {
+    total += Number(arg); // typecasting
+    console.log('arg:', Number(arg), 'Total:', total);
+  }
 }
 
 // print out the total
-console.log("Total:", total)
+console.log('Total:', total);
