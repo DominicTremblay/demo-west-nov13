999d17bc08d09db3714a5422453f8a53782c55da
Dominic Tremblay <dominictremblay24@gmail.com>
2023-11-14 14:38:27 -0500

    solving the core of the sum problem

--- sum.js
+++ sum.js
@@ -4,4 +4,33 @@
 // skip it. Do support negative numbers though. If any argument
 // is not a number, output an error message. We need at least 2 arguments.
 
-// take in an unlimited number of command line arguments
+// Extract the command line arguments
+// What's the type of our command line args?
+const args = process.argv.slice(2);
+console.log('args:', args);
+
+// Edge cases: We need at least 2 arguments. => output error message
+
+// Edge cases: We need to check if they are whole numbers
+// Edge cases: If any argument is not a number, output an error message.
+
+// iterate through the args
+// c-style loop
+// for of loop, forEach loop
+
+// for (let i = 0; i < args.length; i++) {
+
+//   console.log("i:", i, "arg:", args[i])
+
+// }
+// accumulator
+let total = 0;
+
+for (let arg of args) {
+  // Add each argument
+  total += Number(arg); // typecasting
+  console.log('arg:', Number(arg), 'Total:', total);
+}
+
+// print out the total
+console.log("Total:", total)
